b8c93a34398765d459f31bbb1ac9d42e3005d249
sameer <sdsameer61116@gmail.com>
2024-03-05 19:13:50 +0530

    d

--- src/Subjects/Polity/Executive/index.js
+++ src/Subjects/Polity/Executive/index.js
@@ -1,12 +1,105 @@
 const Executive = [
 	{
-		questionText: 'ff',
-		ans: ['ff'],
+		questionText: 'A.52',
+		ans: ['There shall be president'],
 	},
 	{
-		questionText: 'rr',
-		ans: ['rr'],
+		questionText: 'A.63',
+		ans: ['There shall be vice President'],
 	},
+	{
+		questionText: 'A.74',
+		ans: ['U.C.O.M + PM'],
+	},
+	{
+		questionText: 'A.53',
+		ans: ['The Executive powers is vested President', 'Supreme commander of Defence'],
+	},
+	{
+		questionText: 'A.54',
+		ans: ['Election of President'],
+	},
+	{
+		questionText: 'A.56',
+		ans: ['Term of president'],
+	},
+	{
+		questionText: 'A.62',
+		ans: ['The Term of Acting President should not more than 6 months'],
+	},
+	{
+		questionText: 'A.60',
+		ans: ['Oath of president'],
+	},
+	{
+		questionText: 'A.71',
+		ans: ['Challenge of election of president and vice president'],
+	},
+	{
+		questionText: 'A.61',
+		ans: ['Impeachment of president'],
+	},
+	{
+		questionText: 'A.78',
+		ans: ['It is the duty of PM to tell all info about Govt decisions,legislation'],
+	},
+	{
+		questionText: 'A.111',
+		ans: ['Veto'],
+	},
+	{
+		questionText: 'A.123',
+		ans: ['Ordinace power of president'],
+	},
+	{
+		questionText: 'A.213',
+		ans: ['Ordinace power of Governer'],
+	},
+	{
+		questionText: 'A.280',
+		ans: ['Appointment of Finacial comission member'],
+	},
+	{
+		questionText: 'A.112',
+		ans: ['Annual finacial statement '],
+	},
+	{
+		questionText: 'A.72',
+		ans: ['Pardon power'],
+	},
+	{
+		questionText: 'A.64',
+		ans: ['Vice president is ex officio chairman of Rajya sabha'],
+	},
+	{
+		questionText: 'A.65',
+		ans: ['VP as Acting President'],
+	},
+	{
+		questionText: 'A.66',
+		ans: ['Election of vice president'],
+	},
+	{
+		questionText: 'A.67',
+		ans: ['Term of office of vice president'],
+	},
+	{
+		questionText: 'A.68',
+		ans: ['Vacancy and Casual vacancy of VP'],
+	},
+	{
+		questionText: 'A.69 ',
+		ans: ['Oath of VP'],
+	},
+	// {
+	// 	questionText: '',
+	// 	ans: [''],
+	// },
+	// {
+	// 	questionText: '',
+	// 	ans: [''],
+	// },
+
 ];
 
 export default Executive;
